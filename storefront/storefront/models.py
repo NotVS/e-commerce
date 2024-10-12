95395e3806712ff52480bff5f61101a67906eba3
NotVS <154003028+NotVS@users.noreply.github.com>
2024-10-12 16:01:13 +0400

    Update models.py

--- storefront/storefront/models.py
+++ storefront/storefront/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 class Item(models.Model):
-  title = models.CharField
+  title = models.CharField(max_length = 50)
+
+  def __str__(self):
+    return self.title
 
 class OrderItem(models.Model):
   pass
 
 class ShoppingCart(models.Model):
-  pass
+  user = 
+
+  def __str__(self):
+    return self.user.username
   
